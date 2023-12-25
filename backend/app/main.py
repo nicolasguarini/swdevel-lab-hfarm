40092367cbd0c25585f91dd051e7e2a1192a4281
Nicol Emanuele <nicol.eeemanuele@icloud.com>
2023-12-26 00:55:50 +0100

    Add advanced_research endpoint :)

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, Query, HTTPException
 from fastapi.responses import JSONResponse
 from datetime import datetime
 import pandas as pd
-from .mymodules.utils import top_wines_by_rating, wines_by_least_recent_year, wines_by_recent_year, wines_by_type
+from .mymodules.utils import top_wines_by_rating, wines_by_least_recent_year, wines_by_recent_year, filter_wines
 
 
 app = FastAPI()
@@ -25,6 +25,7 @@ df_sparkling["type"] = "sparkling"
 df_white["type"] = "white"
 
 df_wines = pd.concat([df_red, df_rose, df_sparkling, df_white])
+df_wines.columns = map(str.lower, df_wines.columns)
 
 @app.get('/top-wines')
 def get_most_rated_wines(limit: int = 10):
@@ -65,19 +66,50 @@ def get_least_recent_year(limit: int = 10):
     least_recent_wines = wines_by_least_recent_year(df_wines, limit).to_dict(orient='records')
     return JSONResponse(content=least_recent_wines) 
 
-@app.get('/wines-by-type')
-def get_wines_by_type(wine_type, limit: int = 10):
+@app.get('/advanced-search')
+def advanced_search_wines(
+    type: str = Query(None),
+    country: str = Query(None),
+    region: str = Query(None),
+    winery: str = Query(None),
+    rating: float = Query(None),
+    num_ratings: int = Query(None),
+    price: float = Query(None),
+    year: int = Query(None),
+    limit: int = Query(10),
+):
     """
-    Endpoint to get wines by type.
+    Endpoint for advanced search of wines.
 
     Parameters:
-        limit: (optional) an integer representing the max number of wines that has to be returned
-        wine_type: (mandatory) typology of wine [red, rose, sparkling, white]
+        category (optional): Wine category.
+        country (optional): Wine country.
+        region (optional): Wine region.
+        winery (optional): Wine winery.
+        rating (optional): Wine rating.
+        num_ratings (optional): Number of ratings for the wine.
+        price (optional): Wine price.
+        year (optional): Wine year.
+        limit (optional): Max number of wines to return.
+
     Returns:
-        dict: top wines sorted by rating (descending)
+        dict: Wines matching the specified criteria.
     """
-    wines_by_type_result = wines_by_type(df_wines, wine_type, limit).to_dict(orient='records')
-    return JSONResponse(content=wines_by_type_result)
+    filters = {
+        "type": type,
+        "country": country,
+        "region": region,
+        "winery": winery,
+        "rating": rating,
+        "numberofratings": num_ratings,
+        "price": price,
+        "year": year,
+    }
+    result = filter_wines(df_wines, filters).head(limit).to_dict(orient='records')
+
+    print("RESULT", result)
+
+    return JSONResponse(content=result)
 
 @app.get('/')
 def read_root():
@@ -98,6 +130,8 @@ def read_root():
         }
     }
     return JSONResponse(content=info)
+
+
 
 @app.get('/get-date')
 def get_date():
--- backend/app/mymodules/utils.py
+++ backend/app/mymodules/utils.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 def top_wines_by_rating(df_wines, limit=10):
     top_wines = df_wines.sort_values(by="Rating", ascending=False)
     return top_wines.head(limit)
@@ -14,6 +12,55 @@ def wines_by_least_recent_year(df_wines, limit=10):
     least_recent_wines = df_wines.sort_values(by="Year", ascending=True)
     return least_recent_wines.head(limit)
 
-def wines_by_type(df, wine_type, limit=10):
-    filtered_wines = df[df["type"] == wine_type]
-    return filtered_wines.head(limit)
+def filter_by_type(df, type):
+    return df[df["type"] == type]
+
+def filter_by_name(df, name):
+    return df[df["name"].contains(name)]
+
+def filter_by_country(df, country):
+    return df[df["country"] == country]
+
+def filter_by_regiong(df, region):
+    return df[df["region"] == region]
+
+def filter_by_winery(df, winery):
+    return df[df["winery"] == winery]
+
+def filter_by_rating(df, rating):
+    return df[df["rating"] == rating]
+
+def filter_by_num_ratings(df, num_ratings):
+    return df[df["numberofratings"] == num_ratings]
+
+def filter_by_price(df, price):
+    return df[df["price"] == price]
+
+def filter_by_year(df, year):
+    
+    return df[df["year"] == str(year)]
+
+# Main filtering function
+def filter_wines(df_wines, filters):
+    filtered_wines = df_wines.copy()
+    print(filtered_wines.head())
+    for key, value in filters.items():
+        if value is not None and key in df_wines.columns:
+            key_lower = key.lower()  # Convert key to lowercase
+            filter_function = get_filter_function(key_lower)
+            filtered_wines = filter_function(filtered_wines, value)
+
+    print("GUARDA QUI", filtered_wines["region"])
+    return filtered_wines
+
+# Helper function to map filter keys to corresponding filter functions
+def get_filter_function(key):
+    filter_functions = {
+        "type": filter_by_type,
+        "country": filter_by_country,
+        "rating": filter_by_rating,
+        "numberofratings": filter_by_num_ratings,
+        "price": filter_by_price,
+        "year": filter_by_year,
+    }
+    return filter_functions.get(key, lambda df, value: df)  # Default to identity function
