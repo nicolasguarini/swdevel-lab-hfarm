f3560bf00c82a45bdc921788695543e40cdbeb9e
Nicol Emanuele <nicol.eeemanuele@icloud.com>
2024-01-04 16:41:42 +0100

    Add documentation modules

--- backend/app/mymodules/filter_functions.py
+++ backend/app/mymodules/filter_functions.py
@@ -1,15 +1,52 @@
 import pandas as pd
 
 def filter_dataframe(df, column, value):
+    """
+    Filter a DataFrame based on a specific value in a specified column.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame to be filtered.
+        column (str): The name of the column in the DataFrame for filtering.
+        value: The specific value to filter for in the specified column.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame containing only rows where the specified column has the specified value.
+    """
     return df[df[column] == value]
 
 def filter_contains(df, column, value):
+    """
+    Filter a DataFrame based on substring matching in a specified column.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame to be filtered.
+        column (str): The name of the column in the DataFrame for filtering.
+        value: The substring to check for in the specified column.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame containing only rows that match the substring condition.
+    """
     if column == 'year':
+        # For 'year' column, check for an exact match with the provided year (converted to a string)
         return df[df["year"] == str(value)]  
     else:
+        # For other columns, perform a case-insensitive substring match
         return df[df[column].str.contains(value, case=False, na=False)]
 
 def filter_range(df, column, min_value, max_value):
+    """
+    Filter a DataFrame based on a numeric range or exact match for the 'year' column.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame to be filtered.
+        column (str): The name of the column in the DataFrame for filtering.
+        min_value: The minimum value for the range or exact match.
+        max_value: The maximum value for the range.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame containing only rows where the specified column's values fall within the specified range.
+    """
     if column == 'year':
-        df[column] =  pd.to_numeric(df[column], errors='coerce').astype('Int64')
+        # For 'year' column, convert to numeric (ignoring errors for non-numeric values) and filter based on a numeric range
+        df[column] = pd.to_numeric(df[column], errors='coerce').astype('Int64')
     return df[(df[column] >= min_value) & (df[column] <= max_value)]
--- backend/app/mymodules/utils.py
+++ backend/app/mymodules/utils.py
@@ -1,28 +1,76 @@
 from .filter_functions import filter_contains, filter_dataframe, filter_range
 
 def top_wines_by_rating(df_wines, limit=10):
+    """
+    Get the top wines based on rating.
+
+    Parameters:
+        df_wines (pd.DataFrame): DataFrame containing wine information.
+        limit (int): Number of top wines to return (default is 10).
+
+    Returns:
+        pd.DataFrame: Top wines sorted by rating in descending order.
+    """
     return df_wines.sort_values(by="rating", ascending=False).head(limit)
 
 def wines_by_recent_year(df_wines, limit=10):
+    """
+    Get the most recently reviewed wines.
+
+    Parameters:
+        df_wines (pd.DataFrame): DataFrame containing wine information.
+        limit (int): Number of wines to return (default is 10).
+
+    Returns:
+        pd.DataFrame: Most recently reviewed wines sorted by year in descending order.
+    """
+    # Exclude wines with 'N.V.' (non-vintage) in the 'year' column
     df_wines = df_wines[df_wines['year'] != 'N.V.']
     return df_wines.sort_values(by="year", ascending=False).head(limit)
 
 def wines_by_least_recent_year(df_wines, limit=10):
+    """
+    Get the least recently reviewed wines.
+
+    Parameters:
+        df_wines (pd.DataFrame): DataFrame containing wine information.
+        limit (int): Number of wines to return (default is 10).
+
+    Returns:
+        pd.DataFrame: Least recently reviewed wines sorted by year in ascending order.
+    """
+    # Exclude wines with 'N.V.' (non-vintage) in the 'year' column
     df_wines = df_wines[df_wines['year'] != 'N.V.']
     return df_wines.sort_values(by="year", ascending=True).head(limit)
 
 def filter_wines(df_wines, filters):
+    """
+    Filter wines based on specified criteria.
+
+    Parameters:
+        df_wines (pd.DataFrame): DataFrame containing wine information.
+        filters (dict): Dictionary of filters to apply to the DataFrame.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame containing wines that match the specified criteria.
+    """
+    # Create a copy of the original DataFrame for filtering
     filtered_wines = df_wines.copy()
     
+    # Iterate through filters and apply them to the DataFrame
     for column, value in filters.items():
         if value is not None and column in df_wines.columns:
             if isinstance(value, tuple):
+                # If the filter value is a tuple, apply range filtering
                 filtered_wines = filter_range(filtered_wines, column, *value)
             elif column == 'name':
+                # If the filter is on the 'name' column, perform substring matching
                 filtered_wines = filter_contains(filtered_wines, column, value)
             elif column == 'year':
+                # If the filter is on the 'year' column, apply range filtering
                 filtered_wines = filter_range(filtered_wines, column, *value)
             else:
+                # For other columns, apply exact match filtering
                 filtered_wines = filter_dataframe(filtered_wines, column, value)
 
     return filtered_wines
